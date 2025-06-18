ba30c0702d03b4a785204f71a7f5e3f5ba0fe09c
Miguel26112001 <u202220294@upc.edu.pe>
2025-06-18 00:10:05 -0500

    chore: Added Shared Bounded Context

---
+++ UPC.FitWisePlatform.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using EntityFrameworkCore.CreatedUpdatedDate.Extensions;
+using Microsoft.EntityFrameworkCore;
+using UPC.FitWisePlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
+
+namespace UPC.FitWisePlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+
+public class AppDbContext(DbContextOptions options) : DbContext(options)
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder builder)
+    {
+        builder.AddCreatedUpdatedInterceptor();
+        base.OnConfiguring(builder);
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        
+        builder.UseSnakeCaseNamingConvention();
+    }
+}
---
+++ UPC.FitWisePlatform.API/Shared/Infrastructure/Persistence/EFC/Repositories/UnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using UPC.FitWisePlatform.API.Shared.Domain.Repositories;
+using UPC.FitWisePlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+
+namespace UPC.FitWisePlatform.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+public class UnitOfWork(AppDbContext context): IUnitOfWork
+{
+    public async Task CompleteAsync()
+    {
+        await context.SaveChangesAsync();
+    }
+}
