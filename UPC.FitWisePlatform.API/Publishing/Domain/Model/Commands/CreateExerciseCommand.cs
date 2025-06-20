eaac4377bb69dae29ad443e2811227437871b49f
Miguel26112001 <u202220294@upc.edu.pe>
2025-06-20 15:11:57 -0500

    feat: Added Exercise Post Command

---
+++ UPC.FitWisePlatform.API/Publishing/Domain/Model/Commands/CreateExerciseCommand.cs
@@ -0,0 +1,7 @@
+﻿namespace UPC.FitWisePlatform.API.Publishing.Domain.Model.Commands;
+
+public record CreateExerciseCommand(
+    int HealthPlanId,
+    string Name,
+    string Description,
+    string Image);
