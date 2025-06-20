5e044dc23adc5b7f0eb7b1c8058d901dc891bf87
RamiroGuzmanCh <u202217062@upc.edu.pe>
2025-06-20 01:09:30 -0500

    feat: Add IReviewCommandService

---
+++ UPC.FitWisePlatform.API/Reviewing/Domain/Services/IReviewCommandService.cs
@@ -0,0 +1,11 @@
+﻿using UPC.FitWisePlatform.API.Reviewing.Domain.Model.Aggregate;
+using UPC.FitWisePlatform.API.Reviewing.Domain.Model.Commands;
+
+namespace UPC.FitWisePlatform.API.Reviewing.Domain.Services;
+
+public interface IReviewCommandService
+{
+    Task<Review> Handle(CreateReviewCommand command);
+    Task<Review?> Handle(UpdateReviewCommand command);
+    Task<bool> Handle(DeleteReviewCommand command);
+}
