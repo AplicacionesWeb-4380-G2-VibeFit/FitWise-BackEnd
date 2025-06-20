cfb9a59a36f33b492e299e065bc70f46f1771bf7
RamiroGuzmanCh <u202217062@upc.edu.pe>
2025-06-20 00:14:14 -0500

    feat: Add Review.cs

---
+++ UPC.FitWisePlatform.API/Reviewing/Domain/Model/Aggregate/Review.cs
@@ -0,0 +1,39 @@
+﻿using UPC.FitWisePlatform.API.Reviewing.Domain.Model.Entities;
+
+namespace UPC.FitWisePlatform.API.Reviewing.Domain.Model.Aggregate;
+
+public class Review
+{
+    public int Id { get; private set; }
+    public string UserId { get; private set; }
+    public int Score { get; private set; }
+    public string Description { get; private set; }
+    public int HealthPlanId { get; private set; }
+
+    public ICollection<ReviewComment> Comments { get; private set; }
+    public ICollection<ReviewReport> Reports { get; private set; }
+
+    public Review()
+    {
+        UserId = string.Empty;
+        Description = string.Empty;
+        Comments = new List<ReviewComment>();
+        Reports = new List<ReviewReport>();
+    }
+
+    public Review(string userId, int score, string description, int healthPlanId)
+    {
+        UserId = userId;
+        Score = score;
+        Description = description;
+        HealthPlanId = healthPlanId;
+        Comments = new List<ReviewComment>();
+        Reports = new List<ReviewReport>();
+    }
+
+    public void Update(int score, string description)
+    {
+        Score = score;
+        Description = description;
+    }
+}
