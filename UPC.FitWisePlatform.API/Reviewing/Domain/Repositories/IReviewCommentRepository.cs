1d2a9b31f4ec4ccbeb6a80f66904d3afc4a84ee5
RamiroGuzmanCh <u202217062@upc.edu.pe>
2025-06-20 00:40:19 -0500

    feat: Add IReviewCommentRepository.cs

---
+++ UPC.FitWisePlatform.API/Reviewing/Domain/Repositories/IReviewCommentRepository.cs
@@ -0,0 +1,9 @@
+﻿using UPC.FitWisePlatform.API.Reviewing.Domain.Model.Entities;
+using UPC.FitWisePlatform.API.Shared.Domain.Repositories;
+
+namespace UPC.FitWisePlatform.API.Reviewing.Domain.Repositories;
+
+public interface IReviewCommentRepository : IBaseRepository<ReviewComment>
+{
+    Task<IEnumerable<ReviewComment>> FindByReviewIdAsync(int reviewId);
+}
