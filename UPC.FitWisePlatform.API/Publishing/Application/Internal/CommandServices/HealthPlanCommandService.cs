44c58bad4b282babdef5e227b50baae78a2602de
Miguel26112001 <u202220294@upc.edu.pe>
2025-06-19 20:04:19 -0500

    feat: Implemented HealthPlan Services

---
+++ UPC.FitWisePlatform.API/Publishing/Application/Internal/CommandServices/HealthPlanCommandService.cs
@@ -0,0 +1,21 @@
+﻿using UPC.FitWisePlatform.API.Publishing.Domain.Model.Aggregate;
+using UPC.FitWisePlatform.API.Publishing.Domain.Model.Commands;
+using UPC.FitWisePlatform.API.Publishing.Domain.Repositories;
+using UPC.FitWisePlatform.API.Publishing.Domain.Services;
+using UPC.FitWisePlatform.API.Shared.Domain.Repositories;
+
+namespace UPC.FitWisePlatform.API.Publishing.Application.Internal.CommandServices;
+
+public class HealthPlanCommandService(IHealthPlanRepository healthPlanRepository,
+    IUnitOfWork  unitOfWork) : IHealthPlanCommandService
+{
+    public async Task<HealthPlan?> Handle(CreateHealthPlanCommand command)
+    {
+        if (await healthPlanRepository.ExistsByNameAsync(command.Name))
+            throw new Exception("HealthPlan with the same name already exists");
+        var healthPlan = new HealthPlan(command);
+        await healthPlanRepository.AddAsync(healthPlan);
+        await unitOfWork.CompleteAsync();
+        return healthPlan;
+    }
+}
---
+++ UPC.FitWisePlatform.API/Publishing/Application/Internal/QueryServices/HealthPlanQueryService.cs
@@ -0,0 +1,24 @@
+﻿using UPC.FitWisePlatform.API.Publishing.Domain.Model.Aggregate;
+using UPC.FitWisePlatform.API.Publishing.Domain.Model.Queries;
+using UPC.FitWisePlatform.API.Publishing.Domain.Repositories;
+using UPC.FitWisePlatform.API.Publishing.Domain.Services;
+
+namespace UPC.FitWisePlatform.API.Publishing.Application.Internal.QueryServices;
+
+public class HealthPlanQueryService(IHealthPlanRepository healthPlanRepository) : IHealthPlanQueryService
+{
+    public async Task<IEnumerable<HealthPlan>> Handle(GetAllHealthPlansQuery query)
+    {
+        return await healthPlanRepository.ListAsync();
+    }
+
+    public async Task<HealthPlan?> Handle(GetHealthPlanByIdQuery query)
+    {
+        return await healthPlanRepository.FindByIdAsync(query.HealthPlanId);
+    }
+
+    public async Task<IEnumerable<HealthPlan>> Handle(GetHealthPlansByCreatorIdQuery query)
+    {
+        return await healthPlanRepository.FindByCreatorIdAsync(query.CreatorId);
+    }
+}
