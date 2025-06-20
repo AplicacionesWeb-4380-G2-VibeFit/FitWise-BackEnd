f32a111f84bcade77797016e001e46857c6dd182
RamiroGuzmanCh <u202217062@upc.edu.pe>
2025-06-20 01:19:42 -0500

    feat: Add ReviewReportQueryService

---
+++ UPC.FitWisePlatform.API/Reviewing/Application/Internal/QueryServices/ReviewReportQueryService.cs
@@ -0,0 +1,19 @@
+﻿using UPC.FitWisePlatform.API.Reviewing.Domain.Model.Entities;
+using UPC.FitWisePlatform.API.Reviewing.Domain.Model.Queries;
+using UPC.FitWisePlatform.API.Reviewing.Domain.Repositories;
+using UPC.FitWisePlatform.API.Reviewing.Domain.Services;
+
+namespace UPC.FitWisePlatform.API.Reviewing.Application.Internal.QueryServices;
+
+public class ReviewReportQueryService(IReviewReportRepository reportRepository) : IReviewReportQueryService
+{
+    public async Task<IEnumerable<ReviewReport>> Handle(GetAllReviewReportsQuery query)
+    {
+        return await reportRepository.ListAsync();
+    }
+
+    public async Task<IEnumerable<ReviewReport>> Handle(GetReviewReportsByReviewIdQuery query)
+    {
+        return await reportRepository.FindByReviewIdAsync(query.ReviewId);
+    }
+}
