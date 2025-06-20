76830d913ddfdd38c5be1ee0f4ed558017372e09
RamiroGuzmanCh <u202217062@upc.edu.pe>
2025-06-20 01:18:39 -0500

    feat: Add ReviewCommentQueryService

---
+++ UPC.FitWisePlatform.API/Reviewing/Application/Internal/QueryServices/ReviewCommentQueryService.cs
@@ -0,0 +1,19 @@
+﻿using UPC.FitWisePlatform.API.Reviewing.Domain.Model.Entities;
+using UPC.FitWisePlatform.API.Reviewing.Domain.Model.Queries;
+using UPC.FitWisePlatform.API.Reviewing.Domain.Repositories;
+using UPC.FitWisePlatform.API.Reviewing.Domain.Services;
+
+namespace UPC.FitWisePlatform.API.Reviewing.Application.Internal.QueryServices;
+
+public class ReviewCommentQueryService(IReviewCommentRepository commentRepository) : IReviewCommentQueryService
+{
+    public async Task<IEnumerable<ReviewComment>> Handle(GetAllReviewCommentsQuery query)
+    {
+        return await commentRepository.ListAsync();
+    }
+
+    public async Task<IEnumerable<ReviewComment>> Handle(GetReviewCommentsByReviewIdQuery query)
+    {
+        return await commentRepository.FindByReviewIdAsync(query.ReviewId);
+    }
+}
