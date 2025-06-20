78ba74a229b4818b12fd12c38e81fd8a7442a901
RamiroGuzmanCh <u202217062@upc.edu.pe>
2025-06-20 01:32:02 -0500

    feat: Add ReviewReportResource

---
+++ UPC.FitWisePlatform.API/Reviewing/Interfaces/REST/Resources/ReviewReportResource.cs
@@ -0,0 +1,3 @@
+﻿namespace UPC.FitWisePlatform.API.Reviewing.Interfaces.REST.Resources;
+
+public record ReviewReportResource(int Id, int ReviewId, string UserId, string Reason, string Status, DateTime CreatedAt);
