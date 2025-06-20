efe9f035a73758b97d699083d5a38c4432f471c2
RamiroGuzmanCh <u202217062@upc.edu.pe>
2025-06-20 01:24:10 -0500

    feat: Add ReviewCommentRepository, ReviewReportRepository and ReviewRepository

---
+++ UPC.FitWisePlatform.API/Reviewing/Infrastructure/Persistence/EFC/Repositories/ReviewCommentRepository.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UPC.FitWisePlatform.API.Reviewing.Domain.Model.Entities;
+using UPC.FitWisePlatform.API.Reviewing.Domain.Repositories;
+using UPC.FitWisePlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using UPC.FitWisePlatform.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+namespace UPC.FitWisePlatform.API.Reviewing.Infrastructure.Persistence.EFC.Repositories;
+
+public class ReviewCommentRepository(AppDbContext context) : BaseRepository<ReviewComment>(context), IReviewCommentRepository
+{
+    public async Task<IEnumerable<ReviewComment>> FindByReviewIdAsync(int reviewId)
+    {
+        return await Context.Set<ReviewComment>()
+            .Where(c => c.ReviewId == reviewId)
+            .ToListAsync();
+    }
+}
