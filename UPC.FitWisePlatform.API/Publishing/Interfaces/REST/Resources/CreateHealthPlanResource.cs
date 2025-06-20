6500b8e8744d1cfc744995d2ea1dd9ab5de38bb6
Miguel26112001 <u202220294@upc.edu.pe>
2025-06-19 20:04:57 -0500

    feat: Added HealthPlan Resources

---
+++ UPC.FitWisePlatform.API/Publishing/Interfaces/REST/Resources/CreateHealthPlanResource.cs
@@ -0,0 +1,11 @@
+﻿namespace UPC.FitWisePlatform.API.Publishing.Interfaces.REST.Resources;
+
+public record CreateHealthPlanResource(
+    string Name, 
+    string Objective, 
+    decimal PriceAmount, 
+    string PriceCurrency,
+    int DurationValue,
+    string DurationUnit,
+    string Description,
+    int CreatorId);
