99a797bbbbd2c43f7500525467d3029e13f3a4f7
RamiroGuzmanCh <u202217062@upc.edu.pe>
2025-06-20 01:39:08 -0500

    feat: Add CreateReviewReportCommandFromResourceAssembler

---
+++ UPC.FitWisePlatform.API/Reviewing/Interfaces/REST/Transform/CreateReviewReportCommandFromResourceAssembler.cs
@@ -0,0 +1,12 @@
+﻿using UPC.FitWisePlatform.API.Reviewing.Domain.Model.Commands;
+using UPC.FitWisePlatform.API.Reviewing.Interfaces.REST.Resources;
+
+namespace UPC.FitWisePlatform.API.Reviewing.Interfaces.REST.Transform;
+
+public static class CreateReviewReportCommandFromResourceAssembler
+{
+    public static CreateReviewReportCommand ToCommandFromResource(CreateReviewReportResource resource)
+    {
+        return new CreateReviewReportCommand(resource.ReviewId, resource.UserId, resource.Reason);
+    }
+}
