f142fa9c20962778e99b6987458d381b7c530df6
Miguel26112001 <u202220294@upc.edu.pe>
2025-07-01 18:03:29 -0500

    feat: delete valueObjects

--- UPC.FitWisePlatform.API/Publishing/Domain/Model/ValueObjects/Duration.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace UPC.FitWisePlatform.API.Publishing.Domain.Model.ValueObjects;
-
-public record Duration(int Value, string Unit)
-{
-    public Duration( ): this(0, string.Empty) {}
-}
--- UPC.FitWisePlatform.API/Publishing/Domain/Model/ValueObjects/Price.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace UPC.FitWisePlatform.API.Publishing.Domain.Model.ValueObjects;
-
-public record Price(decimal Amount, string Currency)
-{
-    public Price() : this(0, string.Empty) {}
-}
