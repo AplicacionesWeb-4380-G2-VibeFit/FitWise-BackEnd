6c321be643e3adc420aec952ebbcf08b797642d1
Miguel26112001 <u202220294@upc.edu.pe>
2025-07-01 18:26:51 -0500

    feat: added repositories

---
+++ UPC.FitWisePlatform.API/Publishing/Infrastructure/Persistence/EFC/Repositories/HealthPlanMealRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UPC.FitWisePlatform.API.Publishing.Domain.Model.Aggregate;
+using UPC.FitWisePlatform.API.Publishing.Domain.Model.ValueObjects;
+using UPC.FitWisePlatform.API.Publishing.Domain.Repositories;
+using UPC.FitWisePlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using UPC.FitWisePlatform.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+namespace UPC.FitWisePlatform.API.Publishing.Infrastructure.Persistence.EFC.Repositories;
+
+public class HealthPlanMealRepository(AppDbContext context) :
+    BaseRepository<HealthPlanMeal>(context), IHealthPlanMealRepository
+{
+    public async Task<IEnumerable<HealthPlanMeal>> FindByHealthPlanIdAsync(int healthPlanId)
+    {
+        return await Context.Set<HealthPlanMeal>().Where(hpm => hpm.HealthPlanId == healthPlanId).ToListAsync();
+    }
+
+    public async Task<bool> ExistsSameAssignmentOnDayOfWeekAsync(int healthPlanId, int mealId, DayOfWeekType dayOfWeek)
+    {
+        return await Context.Set<HealthPlanMeal>().AnyAsync(hpm =>
+            hpm.HealthPlanId == healthPlanId && hpm.MealId == mealId && hpm.DayOfWeek == dayOfWeek);
+    }
+}
---
+++ UPC.FitWisePlatform.API/Publishing/Infrastructure/Persistence/EFC/Repositories/HealthPlanRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UPC.FitWisePlatform.API.Publishing.Domain.Model.Aggregate;
+using UPC.FitWisePlatform.API.Publishing.Domain.Repositories;
+using UPC.FitWisePlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using UPC.FitWisePlatform.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+namespace UPC.FitWisePlatform.API.Publishing.Infrastructure.Persistence.EFC.Repositories;
+
+public class HealthPlanRepository(AppDbContext context) :
+    BaseRepository<HealthPlan>(context), IHealthPlanRepository
+{
+    public async Task<IEnumerable<HealthPlan>> FindByProfileId(int profileId)
+    {
+        return await Context.Set<HealthPlan>().Where(p => p.ProfileId == profileId).ToListAsync();
+    }
+
+    public async Task<bool> ExistsByPlanNameAsync(string planName)
+    {
+        return await Context.Set<HealthPlan>().AnyAsync(hp => hp.PlanName == planName);
+    }
+}
