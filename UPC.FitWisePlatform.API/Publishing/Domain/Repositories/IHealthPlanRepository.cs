14ae608c77b9dec86232b67b76f7bda9c687c1c1
Miguel26112001 <u202220294@upc.edu.pe>
2025-07-01 18:23:48 -0500

    feat: added interfaces repositories

---
+++ UPC.FitWisePlatform.API/Publishing/Domain/Repositories/IHealthPlanRepository.cs
@@ -0,0 +1,10 @@
+﻿using UPC.FitWisePlatform.API.Publishing.Domain.Model.Aggregate;
+using UPC.FitWisePlatform.API.Shared.Domain.Repositories;
+
+namespace UPC.FitWisePlatform.API.Publishing.Domain.Repositories;
+
+public interface IHealthPlanRepository : IBaseRepository<HealthPlan>
+{
+    Task<IEnumerable<HealthPlan>> FindByProfileId(int profileId);
+    Task<bool> ExistsByPlanNameAsync(string planName);
+}
