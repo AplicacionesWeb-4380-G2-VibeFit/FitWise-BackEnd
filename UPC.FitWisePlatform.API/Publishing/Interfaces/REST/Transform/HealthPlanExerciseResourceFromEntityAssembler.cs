5350a84542f312ced984689baa805ec81b0029c3
Miguel26112001 <u202220294@upc.edu.pe>
2025-07-01 18:34:49 -0500

    feat: added assemblers

---
+++ UPC.FitWisePlatform.API/Publishing/Interfaces/REST/Transform/HealthPlanExerciseResourceFromEntityAssembler.cs
@@ -0,0 +1,18 @@
+﻿using UPC.FitWisePlatform.API.Publishing.Domain.Model.Aggregate;
+using UPC.FitWisePlatform.API.Publishing.Interfaces.REST.Resources;
+
+namespace UPC.FitWisePlatform.API.Publishing.Interfaces.REST.Transform;
+
+public static class HealthPlanExerciseResourceFromEntityAssembler
+{
+    public static HealthPlanExerciseResource ToResourceFromEntity(HealthPlanExercise entity)
+    {
+        if (entity == null)
+        {
+            throw new ArgumentNullException(nameof(entity), "Cannot convert a null HealthPlan entity to a HealthPlanResource.");
+        }
+        
+        return new HealthPlanExerciseResource(entity.Id, entity.HealthPlanId, entity.ExerciseId,
+            entity.DayOfWeek.ToString(), entity.Instructions, entity.Sets, entity.Reps, entity.DurationInMinutes);
+    }
+}
