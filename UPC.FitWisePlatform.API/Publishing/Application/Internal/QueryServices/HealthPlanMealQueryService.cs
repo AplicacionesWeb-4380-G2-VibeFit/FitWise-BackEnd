4d5381afef2a6014fb3e8ce87ac7c8f0949c2ae4
Miguel26112001 <u202220294@upc.edu.pe>
2025-07-01 18:29:40 -0500

    feat: added query services

---
+++ UPC.FitWisePlatform.API/Publishing/Application/Internal/QueryServices/HealthPlanMealQueryService.cs
@@ -0,0 +1,21 @@
+﻿using UPC.FitWisePlatform.API.Publishing.Domain.Model.Aggregate;
+using UPC.FitWisePlatform.API.Publishing.Domain.Model.Queries;
+using UPC.FitWisePlatform.API.Publishing.Domain.Repositories;
+using UPC.FitWisePlatform.API.Publishing.Domain.Services;
+
+namespace UPC.FitWisePlatform.API.Publishing.Application.Internal.QueryServices;
+
+public class HealthPlanMealQueryService(
+    IHealthPlanMealRepository healthPlanMealRepository) : IHealthPlanMealQueryService
+
+{
+    public async Task<HealthPlanMeal?> Handle(GetHealthPlanMealByIdQuery query)
+    {
+        return await healthPlanMealRepository.FindByIdAsync(query.Id);
+    }
+
+    public async Task<IEnumerable<HealthPlanMeal>> Handle(GetHealthPlanMealsByHealthPlanIdQuery query)
+    {
+        return await healthPlanMealRepository.FindByHealthPlanIdAsync(query.HealthPlanId);
+    }
+}
