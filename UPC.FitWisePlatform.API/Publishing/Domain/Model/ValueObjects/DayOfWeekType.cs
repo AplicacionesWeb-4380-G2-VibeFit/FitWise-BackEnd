66f1d0f39da3742ad1dc0be655556309faabebdb
Miguel26112001 <u202220294@upc.edu.pe>
2025-07-01 18:07:21 -0500

    feat: added value objects

---
+++ UPC.FitWisePlatform.API/Publishing/Domain/Model/ValueObjects/DayOfWeekType.cs
@@ -0,0 +1,14 @@
+﻿namespace UPC.FitWisePlatform.API.Publishing.Domain.Model.ValueObjects;
+
+public enum DayOfWeekType
+{
+    Unknown = 0,
+    Monday = 1,
+    Tuesday = 2,
+    Wednesday = 3,
+    Thursday = 4,
+    Friday = 5,
+    Saturday = 6,
+    Sunday = 7,
+    EveryDay = 8
+}
---
+++ UPC.FitWisePlatform.API/Publishing/Domain/Model/ValueObjects/Duration.cs
@@ -0,0 +1,40 @@
+﻿namespace UPC.FitWisePlatform.API.Publishing.Domain.Model.ValueObjects;
+
+public record Duration
+{
+    public int DurationValue { get; private set; }
+    public DurationUnit DurationType { get; private set; }
+    
+    public Duration() { }
+
+    public Duration(int durationValue, DurationUnit durationType)
+    {
+        if (durationValue <= 0)
+        {
+            throw new ArgumentException("Duration value must be positive and integer.", nameof(durationValue));
+        }
+        if (durationType == DurationUnit.Undefined)
+        {
+            throw new ArgumentException("Duration unit cannot be undefined.", nameof(durationType));
+        }
+        this.DurationValue = durationValue;
+        this.DurationType = durationType;
+    }
+    
+    public override string ToString()
+    {
+        var unitString = this.DurationType.ToString();
+        if (this.DurationValue > 1)
+        {
+            unitString = unitString switch
+            {
+                "Day" => "Days",
+                "Week" => "Weeks",
+                "Month" => "Months",
+                "Year" => "Years",
+                _ => unitString
+            };
+        }
+        return $"{this.DurationValue} {unitString}";
+    }
+}
