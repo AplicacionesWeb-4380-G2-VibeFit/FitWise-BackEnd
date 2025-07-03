f206cf08acbe549f864f1dc33db193d8c698265a
RamiroGuzmanCh <u202217062@upc.edu.pe>
2025-07-02 21:58:02 -0500

    feat: Add ReviewCommands

---
+++ UPC.FitWisePlatform.API/Reviewing/Domain/Model/Commands/CreateReviewCommentCommand.cs
@@ -0,0 +1,3 @@
+﻿namespace UPC.FitWisePlatform.API.Reviewing.Domain.Model.Commands;
+
+public record CreateReviewCommentCommand(int ReviewId, string UserId, string Content);
---
+++ UPC.FitWisePlatform.API/Reviewing/Domain/Model/Commands/UpdateReviewReportStatusCommand.cs
@@ -0,0 +1,3 @@
+﻿namespace UPC.FitWisePlatform.API.Reviewing.Domain.Model.Commands;
+
+public record UpdateReviewReportStatusCommand(int Id, string Status);
