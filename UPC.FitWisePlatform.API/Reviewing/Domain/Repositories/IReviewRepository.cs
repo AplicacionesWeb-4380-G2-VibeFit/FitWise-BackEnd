782ff6f420fbb140bca59800961f6c84f647b848
RamiroGuzmanCh <u202217062@upc.edu.pe>
2025-07-02 21:59:51 -0500

    feat: Add Review Repositories

---
+++ UPC.FitWisePlatform.API/Reviewing/Domain/Repositories/IReviewRepository.cs
@@ -0,0 +1,9 @@
+﻿using UPC.FitWisePlatform.API.Reviewing.Domain.Model.Aggregate;
+using UPC.FitWisePlatform.API.Shared.Domain.Repositories;
+
+namespace UPC.FitWisePlatform.API.Reviewing.Domain.Repositories;
+
+public interface IReviewRepository : IBaseRepository<Review>
+{
+    Task<IEnumerable<Review>> FindByHealthPlanIdAsync(int healthPlanId);
+}
