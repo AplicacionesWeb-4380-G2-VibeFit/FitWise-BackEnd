43c77ba8f59b53c09aadf3f384c2eccec6cc9150
RamiroGuzmanCh <u202217062@upc.edu.pe>
2025-07-02 22:04:14 -0500

    feat: Add Report command serice

--- UPC.FitWisePlatform.API/Reviewing/Application/Internal/CommandServices/ReviewReportCommandService.cs
+++ UPC.FitWisePlatform.API/Reviewing/Application/Internal/CommandServices/ReviewReportCommandService.cs
@@ -1,6 +1,29 @@
-﻿namespace UPC.FitWisePlatform.API.Reviewing.Application.Internal.CommandServices;
+﻿using UPC.FitWisePlatform.API.Reviewing.Domain.Model.Commands;
+using UPC.FitWisePlatform.API.Reviewing.Domain.Model.Entities;
+using UPC.FitWisePlatform.API.Reviewing.Domain.Repositories;
+using UPC.FitWisePlatform.API.Reviewing.Domain.Services;
+using UPC.FitWisePlatform.API.Shared.Domain.Repositories;
 
-public class ReviewReportCommandService
+namespace UPC.FitWisePlatform.API.Reviewing.Application.Internal.CommandServices;
+
+public class ReviewReportCommandService(IReviewReportRepository reportRepository, IUnitOfWork unitOfWork) : IReviewReportCommandService
 {
-    
+    public async Task<ReviewReport> Handle(CreateReviewReportCommand command)
+    {
+        var report = new ReviewReport(command.ReviewId, command.UserId, command.Reason);
+        await reportRepository.AddAsync(report);
+        await unitOfWork.CompleteAsync();
+        return report;
+    }
+
+    public async Task<ReviewReport?> Handle(UpdateReviewReportStatusCommand command)
+    {
+        var report = await reportRepository.FindByIdAsync(command.Id);
+        if (report == null) return null;
+
+        report.UpdateStatus(command.Status);
+        reportRepository.Update(report);
+        await unitOfWork.CompleteAsync();
+        return report;
+    }
 }
