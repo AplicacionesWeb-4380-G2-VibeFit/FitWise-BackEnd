d9c6505941f7615ef1b9031b9178b5e5423d2dbc
RamiroGuzmanCh <u202217062@upc.edu.pe>
2025-07-02 22:26:37 -0500

    feat: Add ReviewRepository

---
+++ UPC.FitWisePlatform.API/Reviewing/Infrastructure/Persistence/EFC/Repositories/ReviewRepository.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UPC.FitWisePlatform.API.Reviewing.Domain.Model.Aggregate;
+using UPC.FitWisePlatform.API.Reviewing.Domain.Repositories;
+using UPC.FitWisePlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using UPC.FitWisePlatform.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+namespace UPC.FitWisePlatform.API.Reviewing.Infrastructure.Persistence.EFC.Repositories;
+
+public class ReviewRepository(AppDbContext context) : BaseRepository<Review>(context), IReviewRepository
+{
+    public async Task<IEnumerable<Review>> FindByHealthPlanIdAsync(int healthPlanId)
+    {
+        return await Context.Set<Review>()
+            .Where(r => r.HealthPlanId == healthPlanId)
+            .ToListAsync();
+    }
+
+    public new async Task<Review?> FindByIdAsync(int id)
+    {
+        return await Context.Set<Review>()
+            .Include(r => r.Comments)
+            .Include(r => r.Reports)
+            .FirstOrDefaultAsync(r => r.Id == id);
+    }
+
+    public new async Task<IEnumerable<Review>> ListAsync()
+    {
+        return await Context.Set<Review>()
+            .Include(r => r.Comments)
+            .Include(r => r.Reports)
+            .ToListAsync();
+    }
+}
